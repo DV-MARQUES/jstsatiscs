83c8e4aad098d1550efbce8081e333f24f154702
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-05-05 10:13:32 -0300

    Finalizado aula 2 logica

--- src/App.java
+++ src/App.java
@@ -1,55 +1,29 @@
 import javax.swing.JOptionPane;
-
 public class App {
-    public static void main(String[] args) throws Exception {
-        // ternário  ?   :
-       
-        /* int op = Integer.parseInt(JOptionPane.showInputDialog("Qual o resultado de uma população? \n 1-Parâmetro \n 2-Estátitisca"));
-        if(op == 1){
-            JOptionPane.showMessageDialog(null,
-            "Certa Resposta");
-     }else{
-        JOptionPane.showMessageDialog(null, "Resposta Errada",
-        "JStatístics",JOptionPane.ERROR_MESSAGE);
-     }  */ 
-     
-     /*
-        Double op = Double.parseDouble(JOptionPane.showInputDialog("Considerando os números 30,50,80,90 Digite o valor de média aritmética simples?"));
-        if(op == 62.5){
-            JOptionPane.showMessageDialog(null,
-            "Certa Resposta");
-     }else{
-        JOptionPane.showMessageDialog(null, "Resposta Errada",
-        "JStatístics",JOptionPane.ERROR_MESSAGE);
-     }             */
-        Double op = Double.parseDouble(JOptionPane.showInputDialog("Considerado os valores 10,40,50,80,10. Digite qual é a moda?"));
-        if(op == 10){
-            JOptionPane.showMessageDialog(null,
-            "Certa Resposta");
-     }else{
-        JOptionPane.showMessageDialog(null, "Resposta Errada",
-        "JStatístics",JOptionPane.ERROR_MESSAGE);
-     }            
+    public static void main(String[] args) throws Exception{
+         System.out.println("Aula3- Switch e Loops");
+         // O switch é uma estrutura de seleção múltipla
+         // Um if mais organizado
+         // Escolha
+          // Caso
+        // Para resolver listas ou constantes
+          //Menu
+           //opcao1
+           //opcao2 
+        int op = Integer.parseInt(JOptionPane.showInputDialog("Digite a opção: 1-Estatítica Descritiva 2-Probabilidade 3-Modelos Probabilísticos"));
+        switch(op){
+            case 1:JOptionPane.showMessageDialog( null, 
+            "Estatística Descritiva");break;
 
-     }  
+            case 2:JOptionPane.showMessageDialog( null, 
+            "Probabilidade");break;
+
+            case 3:JOptionPane.showMessageDialog( null, 
+            "Modelos Probabilísticos");break;
+
+            default:
+            JOptionPane.showMessageDialog(null, 
+             "Opção Invalida");break;
         }
-          
-    
-    // if else
-    // Exercícios(para cada exercício crie um if)
-    /*
-     * Considerando os números 30,50,80,90 Digite o valor de média aritmética simples?
-     * Gabarito: 62,5
-     * Considerado os valores 10,40,50,80,10. Digite qual é a moda?
-     * Gabarito: 10
-     * Qual a medida de têndencia central que mostra o centro da amostra? 
-     * mediana
-     * 
-     * String nome;
-     * if nome.equals("mediana")
-     * 
-     * double resposta = Double.parseDoble()
-     * 10 minutos
-     * 30 minutos
-     * 10:42 -- 11:12 
-     */
+    }  
+} 
