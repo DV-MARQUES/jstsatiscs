b3b76f5062d2bef7cb1a342a0532519c62479982
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-05-05 11:53:19 -0300

    Finalizado aula3 logica

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ public class App {
         System.out.println("Aula3 - Switch e Loops");
 
         // Menu com switch
-        /* 
+        
         int op = Integer.parseInt(JOptionPane.showInputDialog(
             "Digite a opção:\n1 - Estatística Descritiva\n2 - Probabilidade\n3 - Modelos Probabilísticos"
         ));
@@ -25,8 +25,28 @@ public class App {
                 break;
         }
 
+        String escolha;
+        do{
+            System.out.println( "Deseja escolher outra opção");
+            escolha = JOptionPane.showInputDialog("Digite s-sim ou n-não");
+        }while(escolha != "n"); {
+            switch (op) {
+                case 1:
+                    JOptionPane.showMessageDialog(null, "Estatística Descritiva");
+                    break;
+                case 2:
+                    JOptionPane.showMessageDialog(null, "Probabilidade");
+                    break;
+                case 3:
+                    JOptionPane.showMessageDialog(null, "Modelos Probabilísticos");
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção Inválida");
+                    break;
+        }
+
         JOptionPane.showMessageDialog(null, "Encerrado Sistema");
-        */
+        
 
         // Loop while
         /* 
@@ -36,15 +56,18 @@ public class App {
             contador = contador + 1;
         }
         */
+        /* 
         for(int cont=1;cont<=10;cont++){
             System.out.println(cont);
         }
-        
+        */
+
         String sair;
         do{
             System.out.println( "sair: s ou n");
             sair = JOptionPane.showInputDialog("Digite s-sim ou n-não");
         }while(sair != "n");
     }
+}
 }
 
