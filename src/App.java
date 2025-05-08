2a0cf0da1e810f42bf98972a5c085d21ccb479e2
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-05-08 11:38:47 -0300

    Finalizado aula6 logica

--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Alô Git!");
+        //Classe é um conjunto de objetos
+        //nosso objetivo nesse código é entender a diferença 
+        //entre método com o tipo de void
+        //Criando dados da Vaca
+        Mamifero m1 = new Mamifero();
+        m1.setNomeCientifico("Bos Taurus");
+        JOptionPane.showMessageDialog(null, "Nome Cientifico"+m1.getNomeCientifico());
+
+        //Crinado dados do Canguru
+        Mamifero m2 = new Mamifero();
+        m2.setNomeCientifico("Macropus fuliginosus");
+        JOptionPane.showMessageDialog(null, "Nome Cientifico"+m2.getNomeCientifico());
+
+        //Criando dados do Peixe-Boi
+        Mamifero m3 = new Mamifero();
+        m3.setNomeCientifico("Trichechus");
+        JOptionPane.showMessageDialog(null, "Nome Cientifico"+m3.getNomeCientifico());
     }
 }
