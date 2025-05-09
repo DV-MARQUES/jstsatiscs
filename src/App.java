7ab252195989bb873eb4ea1d055cd40859b3c9cb
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-05-09 10:55:49 -0300

    Finalizado ex2 logica-aula7

--- src/App.java
+++ src/App.java
@@ -1,11 +1,31 @@
 import javax.swing.JOptionPane;
-
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 public class App {
     public static void main(String[] args)throws Exception {
-    // A mensagem a ser exibida
-    String mensagem= "Olá Meu Querido Jardineiro";
+    
+    // A mensagem a ser exibida   
+    String mensagem= "Olá Meu Querido Jardineiro"+"\n"+
+                     "Deseja se Cadastrar";
     
     // A janela a ser exibida
-    JOptionPane.showMessageDialog(null, mensagem);
+    while(true){
+    int opcao = JOptionPane.showConfirmDialog(null, mensagem, "Floralchic" , JOptionPane.YES_NO_OPTION);
+  
+     if(opcao != JOptionPane.YES_OPTION){
+        JOptionPane.showMessageDialog(null, "Até mais Jardineiro");
+        break;
+     }else{
+        String nome = JOptionPane.showInputDialog(null,"Digite seu Nome pequeno Jardineiro: ");
+        String endereco = JOptionPane.showInputDialog(null, "Digite seu Endenreço: ");
+
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter("dadod",true))){
+            writer.write("Nome: "+nome+ "Endereço: "+endereco);
+        }catch(IOException e){
+            JOptionPane.showMessageDialog(null, "Error ao gravar Dados"+ e.getMessage());
+        }
+     }
+    } 
    } 
 }
